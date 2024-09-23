0195ed05f3b4692a824b6543293385313c4844b6
Mian <wangmian618@gmail.com>
2024-09-23 11:22:56 +0200

    Way 2 : using MapCollection

--- src/PokerGame.java
+++ src/PokerGame.java
@@ -1,56 +1,65 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class PokerGame {
-    static ArrayList<String> list = new ArrayList<>();
+
+    // we do not need to sore the card, we only need build corrsponding number
+    private static final HashMap<Integer, String> POKER_MAP = new HashMap<>();
+    private static final ArrayList<Integer> LIST = new ArrayList<>();
+
     // perpare poker cards
     static {
         //扑克牌四个花色
         String[] colors = {"红桃", "黑桃", "方片", "梅花"};
-        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-
-        for (String color : colors) {
-            for (String number : numbers) {
-                list.add(color + number);
+        String[] numbers = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"};
+        int sericalNumber = 0;
+        for (String number : numbers) {
+            for (String color : colors) {
+                POKER_MAP.put(sericalNumber, color + number);
+                LIST.add(sericalNumber);
+                sericalNumber++;
             }
         }
-        list.add("大王");
-        list.add("小王");
-
+        POKER_MAP.put(sericalNumber, "小王");
+        LIST.add(sericalNumber);
+        sericalNumber++;
+        POKER_MAP.put(sericalNumber, "大王");
+        LIST.add(sericalNumber);
     }
-    public  PokerGame() {
-        //shuffle
-        Collections.shuffle(list);
-        ArrayList<String> lord = new ArrayList<>();
-        ArrayList<String> player1 = new ArrayList<>();
-        ArrayList<String> player2 = new ArrayList<>();
-        ArrayList<String> player3 = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
+
+    public PokerGame() {
+        // shuffle
+        Collections.shuffle(LIST);
+        // deal
+        TreeSet<Integer> lord = new TreeSet<>();
+        TreeSet<Integer> player1 = new TreeSet<>();
+        TreeSet<Integer> player2 = new TreeSet<>();
+        TreeSet<Integer> player3 = new TreeSet<>();
+        for (int i = 0; i < LIST.size(); i++) {
             if (i <= 2) {
-                lord.add(list.get(i));
-                continue;
-            }
-            if (i % 3 == 0) {
-                player1.add(list.get(i));
+                lord.add(LIST.get(i));
+            } else if (i % 3 == 0) {
+                player1.add(LIST.get(i));
             } else if (i % 3 == 1) {
-                player2.add(list.get(i));
+                player2.add(LIST.get(i));
             } else {
-                player3.add(list.get(i));
+                player3.add(LIST.get(i));
             }
+
         }
-        lookPoker("lord", lord);
-        lookPoker("player1", player1);
-        lookPoker("player2", player2);
-        lookPoker("player3", player3);
+        lookCards(lord, "地主");
+        lookCards(player1, "玩家1");
+        lookCards(player2, "玩家2");
+        lookCards(player3, "玩家3");
+
+
     }
 
-    public void lookPoker(String player, ArrayList<String> list) {
-        System.out.println(player + ": ");
-        for (String poker : list) {
-            System.out.print(poker + " ");
+
+    private static void lookCards(Collection<Integer> cards, String player) {
+        System.out.print(player + ": ");
+        for (Integer card : cards) {
+            System.out.print(POKER_MAP.get(card) + " ");
         }
         System.out.println();
     }
-
-
 }
